9f41ab126c067e5f44882d549a1b637442651f30
fjp-j <fjp-j@192.168.0.4>
2020-09-06 22:02:46 -0300

    bytebank-herdado

---
+++ bytebank-herdado/src/Designer.java
@@ -0,0 +1,8 @@
+// Designer é um funcionario, Designer herda da class Funcionario
+public class Designer extends Funcionario{
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Designer");
+		return 200;
+	}
+}
